1eef5c7b761f049cf65f15ac23876c0abe95073c
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:52:51 +0530

    Ship as ZenX-OS based on LOS
    * While LOS is base ROM, there are/will be additions over LOS.
      Therefore, rebrand and ship it as ZenX-OS ROM.

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,17 +4,12 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# ZenX-OS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
-
-# LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.zenx.build.version=$(LINEAGE_VERSION) \
+    ro.zenx.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.zenx.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(ZENX_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_BUILD_PROPERTIES += \
