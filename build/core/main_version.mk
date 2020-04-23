4d41d4c11b278483d0d72fb6dfa0d633ef6f9638
Jackeagle <jackeagle102@gmail.com>
2020-04-23 05:11:20 +0200

    vendor: Remove unwanted status prop

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,7 +1,6 @@
 ADDITIONAL_BUILD_PROPERTIES += \
   ro.zenx.version=$(ZENX_DISPLAY_VERSION) \
   ro.zenx.build.status=$(ZENX_BUILDTYPE) \
-  ro.zenx.releasetype=$(ZENX_BUILDTYPE) \
   ro.zenx.changelog.version=Changelog-$(ZENX_VERSION) \
   ro.zenx.fingerprint=$(ZENX_FINGERPRINT) \
   ro.zenx.static.version=$(ZENX_VERSION_STATIC)
