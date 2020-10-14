9a78718ec6708897ea2fb023cb62dafd765d5d60
Pranav Vashi <neobuddy89@gmail.com>
2020-10-14 21:30:18 +0530

    config: Enable blur on most devices
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -6,3 +6,8 @@ PRODUCT_SIZE := full
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
+
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
